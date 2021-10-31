fb779c0c109b55c3c6c53d2c2b91d0776ebb8560
Harsh Vijay Bathija <37671963+HarshBathija@users.noreply.github.com>
2021-10-31 17:55:48 -0400

    Updating file

--- Controller/home.py
+++ Controller/home.py
@@ -65,18 +65,18 @@ def logout():
 #@login_required
 def add_New():
     #print(request.method)
-    if request.method == "POST":
-        company_name = request.form['fullname']
-        location = request.form['location_text']
-        Job_Profile = request.form['text']
-        salary = request.form['sal']
-        user = request.form['user']
-        password = request.form['pass']
-        email = request.form['user_email']
-        sec_question = request.form['starting_date']
-        sec_answer = request.form['starting_date']
-        notes = request.form['notes']
-        date_applied = request.form['starting_date']
+
+    company_name = request.form['fullname']
+    location = request.form['location_text']
+    Job_Profile = request.form['text']
+    salary = request.form['sal']
+    user = request.form['user']
+    password = request.form['pass']
+    email = request.form['user_email']
+    sec_question = request.form['starting_date']
+    sec_answer = request.form['starting_date']
+    notes = request.form['notes']
+    date_applied = request.form['starting_date']
 
     print("Adding New...")
     s_email(company_name,location, Job_Profile,salary, user,password,email,sec_question,sec_answer,notes,date_applied)
