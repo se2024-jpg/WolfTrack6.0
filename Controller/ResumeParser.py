1f5ac04485f5ae39f96ef6df502df43999afb524
aditya4196 <32817064+aditya4196@users.noreply.github.com>
2021-11-01 23:17:06 -0400

    Resume pdf and doc file parser

---
+++ Controller/ResumeParser.py
@@ -0,0 +1,49 @@
+import io
+import pdfminer
+from pdfminer.converter import TextConverter
+from pdfminer.pdfinterp import PDFPageInterpreter
+from pdfminer.pdfinterp import PDFResourceManager
+from pdfminer.pdfpage import PDFPage
+#Docx resume
+import docx2txt
+#Wordcloud
+import re
+import numpy as np  
+import operator
+import nltk
+from nltk.tokenize import word_tokenize 
+from nltk.corpus import stopwords
+nltk.download('stopwords')
+nltk.download('punkt')
+set(stopwords.words('english'))
+from wordcloud import WordCloud
+from nltk.probability import FreqDist
+import matplotlib.pyplot as plt
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+
+def read_pdf_resume(pdf_doc):
+    resource_manager = PDFResourceManager()
+    fake_file_handle = io.StringIO()
+    converter = TextConverter(resource_manager, fake_file_handle)
+    page_interpreter = PDFPageInterpreter(resource_manager, converter)
+    with open(pdf_doc, 'rb') as fh:
+        for page in PDFPage.get_pages(fh, caching=True,check_extractable=True):           
+            page_interpreter.process_page(page)     
+        text = fake_file_handle.getvalue() 
+    # close open handles      
+    converter.close() 
+    fake_file_handle.close() 
+    if text:     
+        return text
+
+def read_word_resume(word_doc):
+     resume = docx2txt.process(word_doc)
+     resume = str(resume)
+     #print(resume)
+     text =  ''.join(resume)
+     text = text.replace("\n", "")
+     if text:
+         return text
+
+
