fd7d7f635ea536e7ea9aea291828ce3c925f22f1
Shonil Bhide <bhideshonil@gmail.com>
2023-11-27 01:27:26 -0500

    fixing test cases

---
+++ Controller/data.py
@@ -0,0 +1,35 @@
+
+data = {
+    "wishlist": ["Microsoft", "Google", "Apple", "Lyft"],
+    "inprogress": ["Twitter", "NetApp", "Facebook"],
+    "applied": ["Amazon", "Pearson"],
+    "offers": ["Perfios", "Reddit"]
+}
+
+upcoming_events = [
+    {"duedate": "28th Sept, 2021",
+     "company": "Apple"
+     },
+    {"duedate": "19th Dec, 2021",
+     "company": "Microsoft"
+     },
+    {"duedate": "21st Dec, 2021",
+     "company": "Amazon"
+     },
+    {"duedate": "21st Dec, 2021",
+     "company": "Amazon"
+     },
+    {"duedate": "21st Dec, 2021",
+     "company": "Amazon"
+     }
+]
+
+profile = {
+    "name": "Elliot Anderson",
+    "Location": "Raleigh, NC",
+    "phone_number": "",
+    "social": {
+            "linkedin": "www.linkedin.com/in/surajdm",
+
+    }
+}
