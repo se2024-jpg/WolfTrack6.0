1c1ba8749a60eb211483d85557e1da42c5d2217d
Joel Jogy George <joeljogy07@gmail.com>
2024-10-31 21:53:14 -0400

    Refactored data.py and updated resume analysis script for clarity

--- Controller/ResumeParser.py
+++ Controller/ResumeParser.py
@@ -1,7 +1,7 @@
 '''
 MIT License
 
-Copyright (c) 2023 Shonil B, Akshada M, Rutuja R, Sakshi B
+Copyright (c) 2024 Girish G N, Joel Jogy George, Pravallika Vasireddy
 
 Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
 
@@ -10,156 +10,81 @@ The above copyright notice and this permission notice shall be included in all c
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 '''
 
-import io
-import pdfminer
-from pdfminer.converter import TextConverter
-from pdfminer.pdfinterp import PDFPageInterpreter
-from pdfminer.pdfinterp import PDFResourceManager
-from pdfminer.pdfpage import PDFPage
-# Docx resume
-import docx2txt
-import PyPDF2
-# Wordcloud
+import os
 import re
-import numpy as np
-import operator
+import io
+import cv2
 import nltk
+import numpy as np
+import PyPDF2
+import docx2txt
+import pytesseract
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
-
-nltk.download('stopwords')
-nltk.download('punkt')
-set(stopwords.words('english'))
-from wordcloud import WordCloud
 from nltk.probability import FreqDist
-import matplotlib.pyplot as plt
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
-import cv2
-import pytesseract
+from wordcloud import WordCloud
+import matplotlib.pyplot as plt
 
+# Download required NLTK data files if not already present
+nltk.download('stopwords', quiet=True)
+nltk.download('punkt', quiet=True)
 
-# def read_pdf_resume(pdf_doc):
-#     resource_manager = PDFResourceManager()
-#     fake_file_handle = io.StringIO()
-#     converter = TextConverter(resource_manager, fake_file_handle)
-#     page_interpreter = PDFPageInterpreter(resource_manager, converter)
-#     with open(pdf_doc, 'rb') as fh:
-#         for page in PDFPage.get_pages(fh, caching=True,check_extractable=True):
-#             page_interpreter.process_page(page)
-#         text = fake_file_handle.getvalue()
-#     # close open handles
-#     converter.close()
-#     fake_file_handle.close()
-#     if text:
-#         return text
-
-
-def read_image_resume(file):
+def read_image_resume(file: str) -> str:
+    """Extract text from an image resume using Tesseract OCR."""
     image = cv2.imread(file)
     text = pytesseract.image_to_string(image)
-    print(text)
-    if text:
-        return text
+    return text if text else ''
 
+def read_pdf_resume(file: str) -> str:
+    """Extract text from the first page of a PDF resume."""
+    with open(file, 'rb') as pdfFileObj:
+        pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
+        if pdfReader.numPages > 0:
+            pageObj = pdfReader.getPage(0)
+            return pageObj.extractText() or ''
+    return ''
 
-def read_pdf_resume(file):
-    # creating a pdf file object
-    pdfFileObj = open(file, 'rb')
-
-    # creating a pdf reader object
-    pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
-
-    # creating a page object
-    pageObj = pdfReader.getPage(0)
-
-    # extracting text from page
-    text = pageObj.extractText()
-    # closing the pdf file object
-    pdfFileObj.close()
-    print("TEST\n\n\n\n\n", text)
-    return text
-
-
-def read_word_resume(word_doc):
+def read_word_resume(word_doc: str) -> str:
+    """Extract text from a Word document resume."""
     resume = docx2txt.process(word_doc)
-    resume = str(resume)
-    # print(resume)
-    text = ''.join(resume)
-    text = text.replace("\n", "")
-    print(text)
-    if text:
-        return text
+    return resume.replace("\n", "") if resume else ''
 
-
-def clean_job_decsription(jd):
-    ''' a function to create a word cloud based on the input text parameter'''
-    ## Clean the Text
-    # Lower
-    clean_jd = jd.lower()
-    # remove punctuation
-    clean_jd = re.sub(r'[^\w\s]', '', clean_jd)
-    # remove trailing spaces
-    clean_jd = clean_jd.strip()
-    # remove numbers
+def clean_job_description(jd: str) -> list:
+    """Clean the job description for text processing."""
+    clean_jd = re.sub(r'[^\w\s]', '', jd.lower()).strip()
     clean_jd = re.sub('[0-9]+', '', clean_jd)
-    # tokenize
-    clean_jd = word_tokenize(clean_jd)
-    # remove stop words
-    stop = stopwords.words('english')
-    clean_jd = [w for w in clean_jd if not w in stop]
-    return (clean_jd)
+    tokens = word_tokenize(clean_jd)
+    stop_words = set(stopwords.words('english'))
+    return [word for word in tokens if word not in stop_words]
 
+def create_word_cloud(corpus: list):
+    """Generate and display a word cloud from the given text corpus."""
+    frequency_distribution = FreqDist(corpus)
+    word_cloud = WordCloud(width=800, height=800, background_color='white', max_words=500)
+    word_cloud.generate_from_frequencies(frequency_distribution)
+    plt.figure(figsize=(10, 10))
+    plt.imshow(word_cloud, interpolation='bilinear')
+    plt.axis('off')
+    plt.show()
 
-def create_word_cloud(jd):
-    corpus = jd
-    fdist = FreqDist(corpus)
-    # print(fdist.most_common(100))
-    words = ' '.join(corpus)
-    words = words.split()
-
-    # create a empty dictionary
-    data = dict()
-    #  Get frequency for each words where word is the key and the count is the value
-    for word in (words):
-        word = word.lower()
-        data[word] = data.get(word, 0) + 1
-
-        # Sort the dictionary in reverse order to print first the most used terms
-    dict(sorted(data.items(), key=operator.itemgetter(1), reverse=True))
-    word_cloud = WordCloud(width=800, height=800,
-                           background_color='white', max_words=500)
-    word_cloud.generate_from_frequencies(data)
-
-
-def get_resume_score(text):
+def get_resume_score(text: list) -> str:
+    """Calculate similarity score between resume and job description."""
     cv = CountVectorizer(stop_words='english')
     count_matrix = cv.fit_transform(text)
-    count_matrix_array = count_matrix.toarray()
-    print("\nSimilarity Scores:")
+    match_percentage = cosine_similarity(count_matrix.toarray())[0][1] * 100
+    return f"Your resume matches about {round(match_percentage + 50, 2)}% of the job description."
 
-    # get the match percentage
-    matchPercentage = cosine_similarity(count_matrix_array)[0][1] * 100
-    matchPercentage = round(matchPercentage, 2)  # round to two decimal
-
-    print("Your resume matches about " + str(matchPercentage+50) + "% of the job description.")
-    return str(matchPercentage+50)
-
-
-def resume_analyzer(jobtext, file):
+def resume_analyzer(jobtext: str, file: str) -> str:
+    """Analyze a resume against a job description and generate insights."""
     if file.endswith(".pdf"):
-        print("\n\n\n\n\n\nn\n\n")
         resume = read_pdf_resume(file)
-    elif file.endswith(".jpeg") or file.endswith(".jpg") or file.endswith(".png"):
+    elif file.lower().endswith((".jpeg", ".jpg", ".png")):
         resume = read_image_resume(file)
     else:
         resume = read_word_resume(file)
-    job_description = jobtext
-    ## Get a Keywords Cloud
-    clean_jd = clean_job_decsription(job_description)
+
+    clean_jd = clean_job_description(jobtext)
     create_word_cloud(clean_jd)
-    text = [resume, job_description]
-
-    ## Get a Match score
-    return get_resume_score(text)
-
+    return get_resume_score([resume, jobtext])
--- Controller/data.py
+++ Controller/data.py
@@ -1,7 +1,7 @@
 '''
 MIT License
 
-Copyright (c) 2023 Shonil B, Akshada M, Rutuja R, Sakshi B
+Copyright (c) 2024 Girish G N, Joel Jogy George, Pravallika Vasireddy
 
 Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
 
@@ -10,37 +10,29 @@ The above copyright notice and this permission notice shall be included in all c
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 '''
 
-data = {
+# Job application data organized by status
+job_application_data = {
     "wishlist": ["Microsoft", "Google", "Apple", "Lyft"],
-    "inprogress": ["Twitter", "NetApp", "Facebook"],
+    "in_progress": ["Twitter", "NetApp", "Facebook"],
     "applied": ["Amazon", "Pearson"],
     "offers": ["Perfios", "Reddit"]
 }
 
+# Upcoming events related to job applications
 upcoming_events = [
-    {"duedate": "28th Sept, 2021",
-     "company": "Apple"
-     },
-    {"duedate": "19th Dec, 2021",
-     "company": "Microsoft"
-     },
-    {"duedate": "21st Dec, 2021",
-     "company": "Amazon"
-     },
-    {"duedate": "21st Dec, 2021",
-     "company": "Amazon"
-     },
-    {"duedate": "21st Dec, 2021",
-     "company": "Amazon"
-     }
+    {"due_date": "28th Dec, 2024", "company": "Apple"},
+    {"due_date": "29th Dec, 2024", "company": "Microsoft"},
+    {"due_date": "11th Jan, 2024", "company": "Amazon"},
+    {"due_date": "15th Jan, 2024", "company": "Amazon"}, 
+    {"due_date": "1st Feb, 2024", "company": "Amazon"}
 ]
 
-profile = {
+# User profile information
+user_profile = {
     "name": "Elliot Anderson",
-    "Location": "Raleigh, NC",
+    "location": "Raleigh, NC",
     "phone_number": "",
     "social": {
-            "linkedin": "www.linkedin.com/in/surajdm",
-
+        "linkedin": "www.linkedin.com/in/surajdm",
     }
 }
