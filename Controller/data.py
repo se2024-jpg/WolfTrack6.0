3adbed6c308629df92fa3ad8d64d9bd921e99406
Joel Jogy George <joeljogy07@gmail.com>
2024-10-31 21:57:45 -0400

    Fix issue related to dict name for data

--- Controller/data.py
+++ Controller/data.py
@@ -9,30 +9,37 @@ The above copyright notice and this permission notice shall be included in all c
 
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 '''
-
-# Job application data organized by status
-job_application_data = {
+data = {
     "wishlist": ["Microsoft", "Google", "Apple", "Lyft"],
-    "in_progress": ["Twitter", "NetApp", "Facebook"],
+    "inprogress": ["Twitter", "NetApp", "Facebook"],
     "applied": ["Amazon", "Pearson"],
     "offers": ["Perfios", "Reddit"]
 }
 
-# Upcoming events related to job applications
 upcoming_events = [
-    {"due_date": "28th Dec, 2024", "company": "Apple"},
-    {"due_date": "29th Dec, 2024", "company": "Microsoft"},
-    {"due_date": "11th Jan, 2024", "company": "Amazon"},
-    {"due_date": "15th Jan, 2024", "company": "Amazon"}, 
-    {"due_date": "1st Feb, 2024", "company": "Amazon"}
+    {"duedate": "28th Dec, 2024",
+     "company": "Apple"
+     },
+    {"duedate": "29th Dec, 2024",
+     "company": "Microsoft"
+     },
+    {"duedate": "11th Jan, 2024",
+     "company": "Amazon"
+     },
+    {"duedate": "15st Jan, 2024",
+     "company": "Amazon"
+     },
+    {"duedate": "1st Feb, 2024",
+     "company": "Amazon"
+     }
 ]
 
-# User profile information
-user_profile = {
+profile = {
     "name": "Elliot Anderson",
-    "location": "Raleigh, NC",
+    "Location": "Raleigh, NC",
     "phone_number": "",
     "social": {
-        "linkedin": "www.linkedin.com/in/surajdm",
+            "linkedin": "www.linkedin.com/in/surajdm",
+
     }
 }
